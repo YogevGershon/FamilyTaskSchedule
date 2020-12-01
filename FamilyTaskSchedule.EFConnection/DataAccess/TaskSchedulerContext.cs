0094394152d359df99f7e13c62d0253c869c70c6
yogev gershon <yogev.gershon@gmail.com>
2020-12-01 20:29:19 +0200

    Added Entity framework base to start

---
+++ FamilyTaskSchedule.EFConnection/DataAccess/TaskSchedulerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyTaskSchedule.EFConnection.DataAccess
+{
+    public class TaskSchedulerContext : DbContext
+    {
+        public TaskSchedulerContext(DbContextOptions options) : base(options) { }
+    }
+}
