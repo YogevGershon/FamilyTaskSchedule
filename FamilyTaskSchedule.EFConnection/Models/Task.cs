5a0280a3423ec8ae6e96cd9eb68c17011196deec
yogev gershon <yogev.gershon@gmail.com>
2020-12-03 10:16:00 +0200

    Added a 'Models' Folder, and created the 'Task' model

---
+++ FamilyTaskSchedule.EFConnection/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyTaskSchedule.EFConnection.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public DateTime TaskCreationTime { get; set; }
+        public DateTime TaskStart { get; set; }
+        public DateTime TaskEnd { get; set; }
+        public bool IsCompleted { get; set; } = false;
+
+    }
+}
