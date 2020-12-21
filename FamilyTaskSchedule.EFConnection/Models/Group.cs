ef78f515af1371995fd48021f19a5ffebc3fbd35
yogev gershon <yogev.gershon@gmail.com>
2020-12-21 18:04:45 +0200

    Added some models

---
+++ FamilyTaskSchedule.EFConnection/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyTaskSchedule.EFConnection.Models
+{
+    public class Group
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationTime { get; set; }
+        public List<GroupsUsers> GroupsUsers { get; set; }
+
+    }
+}
---
+++ FamilyTaskSchedule.EFConnection/Models/GroupsUsers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyTaskSchedule.EFConnection.Models
+{
+    public class GroupsUsers
+    {
+        public int Id { get; set; }
+        
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        
+        public Guid GroupId { get; set; }
+        public Group Group { get; set; }
+        
+        
+        public Guid RoleId { get; set; }
+        public GroupRoles Role { get; set; }
+    }
+}
